cfc9ce0d566c197a7db019c755046e7af6a533c3
Renato Camilio <renatomcamilio@gmail.com>
2017-04-07 00:48:35 -0700

    Introduce layout contexts — simplify sharing context
    A good example of this usage is whenever we don’t define a default set of meta tags to a specific page, it fallback to the default content, improving our SEO performance

--- website/routes/website.js
+++ website/routes/website.js
@@ -4,20 +4,33 @@
  * @module website/routes/Website
  */
 
-module.exports = function (app) {
+const _ = require('lodash');
+
+
+module.exports = (app) => {
+  const layoutContext = {
+    meta: {
+      description: "We are a volunteer run group of (mostly) developers who are committed to using our technical skills to help others through our mission: To harness the cognitive surplus of the tech community towards building solutions to real problems in the world."
+    }
+  };
+
   /**
    * Home Page
    */
-  app.get('/',
-    function renderHome(req, res) {
-      res.render('home', { name: 'John Doe' });
-    });
+  app.get('/', (req, res) => {
+    const routeContext = {};
+
+    res.render('home', _.assign(layoutContext, routeContext));
+  });
 
   /**
    * About Us
    */
-  app.get('/about-us',
-    function renderAboutUs(req, res) {
-      res.render('about-us', { people: ['Abbey', 'Shahin', 'Massimo', 'Renato'] });
-    });
+  app.get('/about-us', (req, res) => {
+    const routeContext = {
+      people: ['Abbey', 'Shahin', 'Massimo', 'Renato']
+    };
+
+    res.render('about-us', _.assign(layoutContext, routeContext));
+  });
 }
