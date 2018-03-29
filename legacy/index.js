4814ee7966ea3d0aa019612d766e189d0a3351f3
Stephen <slinekermiller@gmail.com>
2018-03-29 23:16:41 +0100

    minimal server

--- legacy/index.js
+++ legacy/index.js
@@ -1,29 +1,6 @@
-/**
- * Dependencies
- */
-const express = require('express');
-const argv    = require('optimist').argv;
-
-/**
- * Server App instance
- */
-const app = express();
-app.set('port', argv.port || 3000);
-
-/**
-* Website App Instance
-*/
 const websiteApp = require('./website/app')({});
+const app = require('express')();
 
-/**
-* Mount Website App on the Server instance
-*/
 app.use(websiteApp);
 
-/**
-* lift up HTTP Server listening on `app.get('port')`
-*/
-//TODO: make it HTTPS
-app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(3000, () => console.log('Express server listening on port 3000'));
