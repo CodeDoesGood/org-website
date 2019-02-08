de9805bd419af663ea52553d803ffa549b21be1e
Stephen Lineker-Miller <slinekermiller@gmail.com>
2019-02-08 08:51:15 +0000

    Package updates
    * Updated the packages but the build process is broken and will require maint.

--- nuxt-project/nuxt.config.js
+++ nuxt-project/nuxt.config.js
@@ -1,36 +1,25 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'CodeDoesGood',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#3B8070' },
 
   build: {
     /*
-    ** Run ESLINT on save
-    */
-    extend(config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/,
-        });
-      }
-    },
-  },
+     ** Run ESLINT on save
+     */
+    extend(config, ctx) {}
+  }
 };
